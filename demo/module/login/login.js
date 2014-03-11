e0b8badc55d16112d7315f5833fc31c9e7017a0f
aladdin <aladdin@aladdin.com>
2014-03-11 10:41:47 +0800

    1、完善登录模块功能

--- demo/module/login/login.js
+++ demo/module/login/login.js
@@ -13,15 +13,43 @@ define(function(require) {
         event.bind(loginButton, 'click', function(thisCom) {
             var loginForm = thisModule.findByKey('login-form');
             var msg = loginForm.getData();
-            msg.act = 'LOGIN';
-            message.send(msg);
+            var validate = true;
+            //必填检测
+            var infoUserEmail = thisModule.findByKey('login-info-user-email');
+            if (msg.userEmail === '') {
+                validate = false;
+                infoUserEmail.setLabel('邮箱不能为空');
+            } else {
+                infoUserEmail.setLabel('');
+            }
+            var infoPassword = thisModule.findByKey('login-info-password');
+            if (msg.password === '') {
+                validate = false;
+                infoPassword.setLabel('密码不能为空');
+            } else {
+                infoPassword.setLabel('');
+            }
+            if (validate) {
+                msg.act = 'LOGIN';
+                message.send(msg);
+            }
         });
         //登录消息处理
         message.listen(loginButton, 'LOGIN', function(thisCom, msg) {
-            if(msg.flag === 'SUCCESS') {
-                
+            var info = thisModule.findByKey('login-info');
+            if (msg.flag === 'SUCCESS') {
+                //登录成功，跳转
+                info.setLabel('登录成功，自动跳转。。。');
             } else {
-                
+                //登录失败
+                switch (msg.flag) {
+                    case 'FAILURE_LOGIN_NOT_EXIST':
+                        info.setLabel('昵称或邮箱表存在');
+                        break;
+                    case 'FAILURE_PASSWORD_ERROR':
+                        info.setLabel('密码错误');
+                        break;
+                }
             }
         });
         //切换到登录页面按钮事件处理
@@ -51,21 +79,21 @@ define(function(require) {
             var msg = registerForm.getData();
             var validate = true;
             //必填检测
-            var infoNickName = thisModule.findByKey('info-nick-name');
+            var infoNickName = thisModule.findByKey('register-info-nick-name');
             if (msg.nickName === '') {
                 validate = false;
                 infoNickName.setLabel('昵称不能为空');
             } else {
                 infoNickName.setLabel('');
             }
-            var infoUserEmail = thisModule.findByKey('info-user-email');
+            var infoUserEmail = thisModule.findByKey('register-info-user-email');
             if (msg.userEmail === '') {
                 validate = false;
                 infoUserEmail.setLabel('邮箱不能为空');
             } else {
                 infoUserEmail.setLabel('');
             }
-            var infoPassword = thisModule.findByKey('info-password');
+            var infoPassword = thisModule.findByKey('register-info-password');
             if (msg.password === '') {
                 validate = false;
                 infoPassword.setLabel('密码不能为空');
@@ -74,7 +102,7 @@ define(function(require) {
             }
             if (validate) {
                 //判断密码和密码确认是否一致
-                var infoCheck = thisModule.findByKey('info-check');
+                var infoCheck = thisModule.findByKey('register-info-check');
                 if (msg.password === msg.check) {
                     infoCheck.setLabel('');
                     //一致
@@ -89,10 +117,27 @@ define(function(require) {
         });
         //注册消息处理
         message.listen(loginButton, 'REGISTER', function(thisCom, msg) {
-            if(msg.flag === 'SUCCESS') {
-                
+            var info = thisModule.findByKey('register-info');
+            if (msg.flag === 'SUCCESS') {
+                //注册成功，跳转到登录页面
+                var registerPanel = thisModule.findByKey('register-panel');
+                if (registerPanel.isVisible()) {
+                    var loginPanel = thisModule.findByKey('login-panel');
+                    registerPanel.hide();
+                    loginPanel.show();
+                    var loginForm = thisModule.findByKey('login-form');
+                    loginForm.setData('userEmail', msg.data.nickName);
+                }
             } else {
-                
+                //登录失败
+                switch (msg.flag) {
+                    case 'FAILURE_USER_EMAIL_USED':
+                        info.setLabel('邮箱已经被使用');
+                        break;
+                    case 'FAILURE_USER_NICK_NAME_USED':
+                        info.setLabel('昵称已经被使用');
+                        break;
+                }
             }
         });
     };
